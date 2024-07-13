b537831d05eb84ed7e10ae82da5d3bdfb72e0ef0
LePhuongTrung <135190672+LPTrungGTN@users.noreply.github.com>
2024-07-13 11:46:04 +0700

    'GET', '/works/:id'

--- todoList/app/routes.php
+++ todoList/app/routes.php
@@ -8,6 +8,6 @@ $router = new Router();
 
 $router->addRoute('GET', '/', CalendarController::class, 'showCalendar');
 
-$router->addRoute('POST', '/save-work', WorkController::class, 'saveWork');
-
+$router->addRoute('POST', '/works', WorkController::class, 'store');
+$router->addRoute('GET', '/works/:id', WorkController::class, 'show');
 return $router;
