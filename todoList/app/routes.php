d07c57541a8a6b5833b3c060ec2866b6b9b4e7eb
LePhuongTrung <135190672+LPTrungGTN@users.noreply.github.com>
2024-07-13 13:45:24 +0700

    add api patch and delete work

--- todoList/app/routes.php
+++ todoList/app/routes.php
@@ -10,4 +10,6 @@ $router->addRoute('GET', '/', CalendarController::class, 'showCalendar');
 
 $router->addRoute('POST', '/works', WorkController::class, 'store');
 $router->addRoute('GET', '/works/:id', WorkController::class, 'show');
+$router->addRoute('PATCH', '/works/:id', WorkController::class, 'update');
+$router->addRoute('DELETE', '/works/:id', WorkController::class, 'delete');
 return $router;
